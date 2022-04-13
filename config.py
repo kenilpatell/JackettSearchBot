7e44125eb907a482cc265bfb98a70163b62b0118
kenilpatell <90301093+kenilpatell@users.noreply.github.com>
2022-04-13 17:55:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,5 @@ class Config(object):
 	API_ID = int(os.environ.get("API_ID", "0"))
 	API_HASH = os.environ.get("API_HASH")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
 	API_KEY = os.environ.get("API_KEY")
 	SERVER_NAME = os.environ.get("SERVER_NAME")
