0242427722493770a38d43c6cebb6597f243b387
kenilpatell <90301093+kenilpatell@users.noreply.github.com>
2022-04-13 17:48:41 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# (c) @KenilPatell
+
+import os
+
+
+class Config(object):
+	API_ID = int(os.environ.get("API_ID", "0"))
+	API_HASH = os.environ.get("API_HASH")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME")
+	API_KEY = os.environ.get("API_KEY")
